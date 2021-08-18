10174a0286556761be87923da785d26c08d602b8
fakeharahman <fakeha126@gmail.com>
2021-08-19 00:48:57 +0530

    passages 1 ny complete

--- txt.py
+++ txt.py
@@ -1,54 +1,41 @@
+#  {{}} -> for the passage
+# [[]] -> macqs
+# ##-> for the year and month
+# && -> for the source
+
 import re
 import json
 # import uuid
-strr=""
+strr = ""
 with open(r'Comprehensions_jan_12.txt', encoding="utf8") as op:
     for line in op:
-        strr+=line
+        strr += line
         # print( line)
-    p=re.findall(r'{{([\s\S]*?)}}', strr)
-    id=1
-    passage_dict={}
-    # for i in p1:
-    #     print(i+"////////////////////////////////////")
-    year=re.findall(r'##([\s\S]*?)##', strr)
+    p = re.findall(r'{{([\s\S]*?)}}', strr)  # {{text}}
+    id = 1
+    passage_dict = {}
+    year = re.findall(r'##([\s\S]*?)##', strr)  # text##
     print(year)
-    source=re.findall(r'&&([\s\S]*?)&&', strr)
+    source = re.findall(r'&&([\s\S]*?)&&', strr)  # &&text&&
     print(len(source))
-    mcqs=re.findall(r'\[\[([\s\S]*?)\]\]', strr)
+    mcqs = re.findall(r'\[\[([\s\S]*?)\]\]', strr)  # [[text]]
     for i in range(len(mcqs)):
-        mcqs[i]+="\n9"
-        q=re.findall(r'[0-9] ([\s\S]*?)\(1\)', mcqs[i])
-        p_dict={"id": id, "passage": p[i].strip(), "source": source[i], "year": year[i], "no_of_questions": len(q)}
-        # for qs in range(len(q)):
-        #     p_dict[f'question_{qs+1}']=q[qs]
-        # print(q)
-        opt=re.findall(r'\([0-9]\) ([\s\S]*?)\n[0-9]', mcqs[i])
+        mcqs[i] += "\n9"
+        # [num]text(1) -> to find questions
+        q = re.findall(r'[0-9] ([\s\S]*?)\(1\)', mcqs[i])
+        p_dict = {"id": id, "passage": p[i].strip(), "source": source[i], "year": year[i], "no_of_questions": len(q)}
+        # (num)text(num) -> to find all options
+        opt = re.findall(r'\([0-9]\) ([\s\S]*?)\n[0-9]', mcqs[i])
         # print(len(opt))
         for j in range(len(opt)):
-            p_dict[f'question_{j+1}']=q[j].replace("\n", " ").strip()
-            opt_arr=(re.compile(r'\([0-9]\)').split(opt[j]))
+            p_dict[f'question_{j+1}'] = q[j].replace("\n", " ").strip()
+            opt_arr = (re.compile(r'\([0-9]\)').split(opt[j])) # split the options around (num)
             for k in range(len(opt_arr)):
-                p_dict[f'option_{k+1}_question_{j+1}']=opt_arr[k].replace("\n", " ").strip()
-        passage_dict[id]=p_dict
-        id+=1
+                p_dict[f'option_{k+1}_question_{j+1}'] = opt_arr[k].replace(
+                    "\n", " ").strip()
+        passage_dict[id] = p_dict
+        id += 1
     # print(passage_dict)
-    y=json.dumps(passage_dict)
+    y = json.dumps(passage_dict)
     with open('data.json', 'w', encoding='utf-8') as f:
         json.dump(passage_dict, f, ensure_ascii=False, indent=4)
-    # print(y)
-
-
-
-    # print(mcqs)
-    # for mcq in mcqs:
-    #     mcq+="\n9"
-    #     q=re.findall(r'[0-9] ([\s\S]*?)\(1\)', mcq)
-    #     # print(q)
-    #     opt=re.findall(r'\([0-9]\) ([\s\S]*?)\n[0-9]', mcq)
-    #     for i in opt:
-    #         opt_arr=(re.compile(r'\([0-9]\)').split(i))
-        
-# strip to remove newline charachter
-# f = open("Comprehensions_jan_12.txt", "r")
-# print(f.read())
